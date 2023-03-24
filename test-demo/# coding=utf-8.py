6af8c86a0a97062abbda201809050b4383194ff3
PIAN-BIUBIU <zy894193304@163.com>
2023-03-24 21:25:31 +0800

    第二次保存

--- test-demo/# coding=utf-8.py
+++ test-demo/# coding=utf-8.py
@@ -9,3 +9,35 @@ result=num1/num2
 print(result)
 round_result=round(result,2)
 print(round_result)
+print(True and True)
+
+a='python'
+print('hello,',a or 'world')
+
+b=''
+print('hello,',b or 'world')
+
+t='special string:\',",\\,\\\\,\\n,\\t)'
+print(t)
+
+x=r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
+
+print(x)
+
+x='hello {2},hello {0},hello {1}'
+b=x.format('World','beijing','aoyun')
+print(b)
+
+w="life is short, {q} {e} {w}."
+qi='you'
+ei='need'
+wi='python'
+r=w.format(q=qi,e=ei,w=wi)
+print(r)
+
+print('这是一句中英文混合的Python')
+print('Hello World!')
+
+sss='AABCDEFGHHIJ'
+print(sss[1:9])
